eaaa0cd3872a63e2bc4c91308d659f464a5d6b95
Julio Gudino <julsgud@gmail.com>
2016-09-02 16:56:00 -0500

    Initial Board, Piece classes. Piece locations ready.

--- bundle.js
+++ bundle.js
@@ -59,31 +59,48 @@
 	var sketch = function sketch(p5) {
 		// make library globally available
 		window.p5 = p5;
-		var font = void 0;
-		var img = void 0;
+
+		// layout
+		var frame = void 0,
+		    gridSize = void 0,
+		    tileSize = void 0;
+		var tileCount = 9;
+
+		// state
+		var solved = void 0;
+
+		// object
+		var grid = void 0;
+		var tiles = {};
 
 		p5.setup = function () {
 			p5.createCanvas(p5.windowWidth, p5.windowHeight);
-			font = p5.loadFont('./assets/Alegreya-Regular.ttf');
-			img = p5.loadImage('./assets/panda.jpg');
+			frame = p5.width / 13;
+			gridSize = p5.width - frame * 2;
+			tileSize = gridSize / tileCount;
+			grid = new Object();
+			setBoard();
 		};
 
 		p5.draw = function () {
 			p5.background(p5.color(252, 182, 157), 85);
+		};
 
-			// image
-			p5.push();
-			p5.translate(p5.width / 2, p5.height / 2);
-			p5.rotate(p5.frameCount / p5.TWO_PI);
-			p5.image(img, p5.mouseX - p5.width / 6, p5.mouseY - p5.mouseY / 4, p5.width / 3, p5.height / 2);
-			p5.pop();
-
-			// text
-			p5.fill(255);
-			p5.textFont(font);
-			p5.textSize(p5.width / 8);
-			p5.textAlign(p5.CENTER);
-			p5.text('Stop it P5 panda', p5.width / 2, p5.height / 2);
+		setBoard = function setBoard() {
+			grid = new Array(tileCount);
+			for (var i = 0; i < tileCount; i++) {
+				grid[i] = new Array(tileCount);
+				for (var j = 0; j < tileCount; j++) {
+					grid[i][j] = new Object();
+					grid[i][j].x = tileCount - 1 - i;
+					console.log(grid[i][j].x);
+					grid[i][j].y = tileCount - 1 - j;
+					console.log(grid[i][j].y);
+				}
+			}
+			emptySpot.x = grid[tileCount - 1][tileCount - 1].x;
+			emptySpot.y = grid[tileCount - 1][tileCount - 1].y;
+			solved = false;
 		};
 	};
 
